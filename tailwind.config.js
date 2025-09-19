0fb109263aaad1bec82bfa7c7076cb3d82cef62a
meric <eric.melv42@gmail.com>
2025-09-18 23:30:01 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{astro,js,ts,jsx,tsx}", // pega todos os arquivos relevantes na pasta src
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
